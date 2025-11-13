451ba8c01cb0ee39ef70506848132120a77ef894
Victor Kahl Petersen <victorkahlpetersen@gmail.com>
2025-11-13 09:51:30 +0100

    Add default values for group

--- app/src/main/java/com/example/upaymimoni/domain/model/Group.kt
+++ app/src/main/java/com/example/upaymimoni/domain/model/Group.kt
@@ -1,9 +1,9 @@
 package com.example.upaymimoni.domain.model
 
 data class Group(
-    val id: String,
-    var groupName: String,
-    var groupImage: String?,
-    var members: List<String>?,
-    var expenses: List<String>?
+    val id: String = "",
+    var groupName: String = "",
+    var groupImage: String? = null,
+    var members: List<String>? = emptyList(),
+    var expenses: List<String>? = emptyList()
 )
